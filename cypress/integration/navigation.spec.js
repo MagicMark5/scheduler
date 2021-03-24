7a554abedbe03152c9f225799c420fa1e455048f
Adam Marsala <adammarsala@hotmail.com>
2021-03-24 16:03:43 -0400

    Install cypress and implement test for clicking Tuesday

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,20 @@
+describe("Navigation", () => {
+  
+  beforeEach(() => {
+    // visit the page
+    cy.visit('/');
+  });
+
+  it("should navigate to Tuesday", () => {
+    // click Tuesday and // check that the li with tuesday has a white background
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+    
+      
+
+  });
+
+
+
+});
