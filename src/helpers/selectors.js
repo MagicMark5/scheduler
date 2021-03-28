78d312f5b644cff494ec73bfbb1bb3df496368b3
Adam Marsala <adammarsala@hotmail.com>
2021-03-27 21:08:02 -0400

    Refactor and dry selectors, replace filter with find and map

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,26 +1,21 @@
-function getAppointmentsForDay(state, day) {
-  const apptObjs = [];
-  const apptsForDay = state.days.length > 0 ? state.days.filter((dateObj) => dateObj.name === day) : [];
-  const apptIDs     = apptsForDay.length > 0 ? apptsForDay[0].appointments : [];
+function getStateForDay(state, day, selector) {
+  // selector parameter will be either appointments or interviewers
+  let objects = [];
+  const thisDate = state.days.find(dateObj => dateObj.name === day);
 
-  for (const id of apptIDs) {
-    apptObjs.push(state.appointments[id]);
-  }
+  // the appointments/interviewers keys are stored as array of ids for the given day
+  objects = thisDate ? thisDate[selector].map(id => state[selector][id]) : [];
   
-  return apptObjs;
+  // return array of interviewers/appointments for the day
+  return objects;
+}
+
+function getAppointmentsForDay(state, day) {
+  return getStateForDay(state, day, "appointments");
 }
 
 function getInterviewersForDay(state, day) {
-  const interviewerObjs = [];
-
-  const thisDate = state.days.length > 0 ? state.days.filter((dateObj) => dateObj.name === day) : [];
-  const intervIDs = thisDate.length > 0 ? thisDate[0].interviewers : [];
-
-  for (const id of intervIDs) {
-    interviewerObjs.push(state.interviewers[id]);
-  }
-  
-  return interviewerObjs;
+  return getStateForDay(state, day, "interviewers");
 }
 
 function getInterview(state, interview) {
